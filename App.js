1082e87f2903a9f90af5937e7d35f6d020ac5360
andrew-zimmer <60802386+andrew-zimmer@users.noreply.github.com>
2020-12-04 11:22:27 -0500

    deleted prior work and added text and styled with flex box

--- App.js
+++ App.js
@@ -1,29 +1,36 @@
 import * as React from 'react';
-import { Text, View, ActivityIndicator, ProgressBarAndroid, Button, Alert, Dimensions, Platform } from 'react-native';
-import Constants from 'expo-constants';
+import { Text, View, StyleSheet} from 'react-native';
 
-const {height, width } = Dimensions.get('window')
 
 export default function App(){
 
-  const onButtonPress = () => {
-    Alert.alert(`${new Date().toLocaleTimeString()} button press`)
-  }
 
   return (
-    <View style={{padding: 50}}>
-      <ProgressBarAndroid
-        progress={.5}
-        styleAttr='Horizontal'
-        indeterminate={false}
-        color='blue'
-      />
-      <ActivityIndicator size='large' color='#61dbfb'/>
-      <Button title="click me" onPress={onButtonPress}/>
-      <Text>{Platform.OS}</Text>
-      <Text>Height: {height}</Text>
-      <Text>Width: {width}</Text>
+    <View style={styles.page}>
+      <Text style={styles.text}>red</Text>
+      <Text style={styles.text}>green</Text>
+      <Text style={styles.text}>blue</Text>
 
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 40,
+    backgroundColor: '#DDD'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 22,
+    color: 'red',
+    backgroundColor: 'yellow',
+    margin: 10,
+    padding: 5
+
+  }
+})
