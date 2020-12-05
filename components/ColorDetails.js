a503b722e731da1fed3d7344a8773fec77dd7824
andrew-zimmer <60802386+andrew-zimmer@users.noreply.github.com>
2020-12-05 12:15:09 -0500

    added functionality to go between pages and to view color details using the color node package

--- components/ColorDetails.js
+++ components/ColorDetails.js
@@ -2,11 +2,25 @@ import React from 'react'
 
 import {View, Text, StyleSheet} from 'react-native'
 
-export default function ColorDetails() {
+import Color from 'color'
+
+export default function ColorDetails({route}) {
+    const {color: name} = route.params
+    const color = Color(name)
+    const textColor = { fontSize: 30, color: color.negate().toString()}
     return (
-        <View style={styles.container}>
-            <Text>
-                Color Details
+        <View style={[styles.container, {backgroundColor: name}]}>
+            <Text style={textColor}>
+                Color Details: {route.params.color}
+            </Text>
+            <Text style={textColor}>
+                {color.rgb().toString()}
+            </Text>
+            <Text style={textColor}>
+                {color.hsl().toString()}
+            </Text>
+            <Text style={textColor}>
+                {color.luminosity().toString()}
             </Text>
         </View>
     )
--- components/ColorList.js
+++ components/ColorList.js
@@ -4,7 +4,7 @@ import ColorButton from './ColorButton'
 import ColorForm from './ColorForm'
 import {useColors} from '../hooks'
 
-export default function ColorList(){
+export default function ColorList({navigation}){
   const [backgroundColor, setBackgroundColor] = useState('blue')
 
   const {colors, addColor} = useColors()
@@ -12,11 +12,15 @@ export default function ColorList(){
   return (
     <>
     <ColorForm onNewColor={addColor} />
-    <FlatList style={[styles.page, {backgroundColor}]}
+    <FlatList style={[styles.page]}
       data={colors}
       renderItem={({item}) => {
         return (
-          <ColorButton key={item.id} color={item.color} onPress={setBackgroundColor}/>
+          <ColorButton
+            key={item.id}
+            color={item.color}
+            onPress={() => navigation.navigate('Details', {color: item.color})}
+          />
         )
       }}
     />
